00babb3cf74a89bd40bae1bf926777941fbc779c
Sani-am <79805957+Sani-am@users.noreply.github.com>
2021-12-12 17:19:11 -0500

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,9 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDSDzozxYZrtno0o5LCiKy6uYig8yHNuZk",
+    authDomain: "kwitter-project-8de18.firebaseapp.com",
+    databaseURL: "https://kwitter-project-8de18-default-rtdb.firebaseio.com",
+    projectId: "kwitter-project-8de18",
+    storageBucket: "kwitter-project-8de18.appspot.com",
+    messagingSenderId: "421812108836",
+    appId: "1:421812108836:web:5e9fbc840a3d34dbcea2b1"
+  };
